9c2adbfda279a45b6d312956aa5dbd436c812b59
Danilo Carlos Marques <marquesdanilocarlos@gmail.com>
2024-01-12 21:26:44 -0300

    Aula 36 - Definição dos tipos de campos para o form

--- src/Form/ProductType.php
+++ src/Form/ProductType.php
@@ -6,27 +6,24 @@ use App\Entity\Category;
 use App\Entity\Product;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
-use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 
 class ProductType extends AbstractType
 {
 
-    public function __construct(
-        private UrlGeneratorInterface $urlGenerator
-    ) {
-    }
-
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('name')
-            ->add('description')
-            ->add('body')
-            ->add('price')
-            ->add('slug')
+            ->add('name', TextType::class)
+            ->add('description', TextType::class)
+            ->add('body', TextareaType::class)
+            ->add('price', IntegerType::class)
+            ->add('slug', TextType::class)
             ->add('categories', EntityType::class, [
                 'class' => Category::class,
                 'choice_label' => 'name',
