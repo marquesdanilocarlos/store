5a5ef6145b092b09b13a2bb1c509911acf86effe
Danilo Carlos Marques <marquesdanilocarlos@gmail.com>
2024-01-13 14:56:50 -0300

    Aula 44 - Reabilitação do autowire em um service com apelido (necessário apontar o namespace para o service com apelido para o autowire funcionar)

--- src/Service/UploadService.php
+++ src/Service/UploadService.php
@@ -8,20 +8,12 @@ class UploadService
 {
 
     public function __construct(
-        private string $uploadDir,
-        private bool $hasLog,
-        private LoggerInterface $logger
     ) {
     }
 
     public function upload()
     {
-        if ($this->hasLog) {
-            $this->logger->info('Log vindo da classe de UPLOAD SERVICE');
-        }
-
-        $hasLog = $this->hasLog ? 'Sim' : 'Não';
-        return "Realização de upload para: {$this->uploadDir}, Tem log? {$hasLog}";
+        return "Realização de upload para";
     }
 
 }
