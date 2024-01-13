58855c28524425b55355db69817aed5dc5ad09fc
Danilo Carlos Marques <marquesdanilocarlos@gmail.com>
2024-01-13 14:21:13 -0300

    Aula 43 - Inabilitação do autowire para um Service e definição dos parâmetros a serem passados caso o autowire esteja desabilitado

--- src/Service/UploadService.php
+++ src/Service/UploadService.php
@@ -2,17 +2,26 @@
 
 namespace App\Service;
 
+use Psr\Log\LoggerInterface;
+
 class UploadService
 {
 
     public function __construct(
-        private string $uploadDir
+        private string $uploadDir,
+        private bool $hasLog,
+        private LoggerInterface $logger
     ) {
     }
 
     public function upload()
     {
-        return "Realização de upload para: {$this->uploadDir}";
+        if ($this->hasLog) {
+            $this->logger->info('Log vindo da classe de UPLOAD SERVICE');
+        }
+
+        $hasLog = $this->hasLog ? 'Sim' : 'Não';
+        return "Realização de upload para: {$this->uploadDir}, Tem log? {$hasLog}";
     }
 
 }
